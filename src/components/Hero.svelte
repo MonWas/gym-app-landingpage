68129a5a998f91c6b02d32a682d3b3c63d7696a6
MonWas <65023903+MonWas@users.noreply.github.com>
2024-02-21 21:00:28 +0100

    finalise hero section, add product section and feature list

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from "./Header.svelte";
   import SectionWrapper from "./SectionWrapper.svelte";
+  import CTA from "../components/CTA.svelte";
 </script>
 
 <SectionWrapper>
@@ -12,8 +13,32 @@
       class="text-fxl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold"
     >
       <span class="text-indigo-400">Gym</span> Training
-      <span class="line-through">Is Hard</span><br />
+      <span class="text-slate-600 line-through">Is Hard</span><br />
       Just Got <span class="text-indigo-400">Easier</span>
     </h2>
+    <p
+      class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full"
+    >
+      Less thinking, more doing. We're the trainer in you pocket full of <span
+        class="italic"
+        >personalized workouts, exercise explanations, analytics</span
+      > and much much more.
+    </p>
+    <CTA />
+    <div class="flex items-center justify-center gap-2 text-base">
+      <p>4.6</p>
+      {#each [0, 1, 2, 3, 4] as index}
+        <div class="grid place-items-center relative">
+          <i class="fa-solid fa-star opacity-0"></i>
+          <div
+            class={"absolute top-0 left-0 grid place-items-center " +
+              (index === 4 ? "w[60%] overflow-hidden" : " ")}
+          >
+            <i class="fa-solid fa-star text-amber-400"></i>
+          </div>
+        </div>
+      {/each}
+      <p>500+</p>
+    </div>
   </div>
 </SectionWrapper>
