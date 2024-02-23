8e3ce34df5b72e052d9a850da1d387bb8cc1204a
MonWas <65023903+MonWas@users.noreply.github.com>
2024-02-23 20:52:29 +0100

    add conversion, contact, faqs & footer content + finalize layout & styling

--- src/components/Review.svelte
+++ src/components/Review.svelte
@@ -1,6 +1,7 @@
 <script>
   import ReviewCard from "./ReviewCard.svelte";
   import SectionWrapper from "./SectionWrapper.svelte";
+  import Stars from "./Stars.svelte";
 
   let lim = true;
 
@@ -78,7 +79,7 @@
   ];
 </script>
 
-<SectionWrapper>
+<SectionWrapper id="reviews">
   <div
     class="flex flex-col gap-10 sm_gap-14 md:gap-24 py-20 flex-1 items-center justify-center"
   >
@@ -94,10 +95,25 @@
     </div>
     <div class="flex flex-col md:flex-row gap-8 w-fit mx-auto">
       <div class="flex flex-col gap-8">
-        {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem}
-          <ReviewCard {reviewItem} />
+        {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem, index}
+          <ReviewCard {reviewItem} left={true} {index} />
         {/each}
       </div>
+      <div class="w-[1px] bg-slate-950 hidden md:flex"></div>
+      <div class="flex flex-col gap-8">
+        {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem, index}
+          <ReviewCard {reviewItem} {index} />
+        {/each}
+      </div>
+    </div>
+    <button on:click={() => (lim = !lim)} class="specialBtn">
+      <p>{lim ? "Show More" : "Show Less"}</p>
+    </button>
+    <div class="flex flex-col gap-10">
+      <p class="mx-auto text-lg sm:text-xl md:text-2xl font-semibold">
+        Trusted by 1,349 Swoldiers
+      </p>
+      <Stars />
     </div>
   </div>
 </SectionWrapper>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
   import Hero from "../components/Hero.svelte";
   import Product from "../components/Product.svelte";
   import Review from "../components/Review.svelte";
+  import FAQ from "../components/FAQ.svelte";
+  import Conversion from "../components/Conversion.svelte";
 </script>
 
 <main class="flex flex-col">
   <Hero />
   <Product />
   <Review />
+  <FAQ />
+  <Conversion />
 </main>
